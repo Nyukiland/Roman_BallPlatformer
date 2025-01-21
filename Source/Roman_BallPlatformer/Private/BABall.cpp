976af92a42ebd2e76ee2a3c40ff126c309af121f
Nyukiland <116567790+Nyukiland@users.noreply.github.com>
2025-01-21 12:23:35 +0100

    [UPD] Base Ball C++

---
+++ Source/Roman_BallPlatformer/Private/BABall.cpp
@@ -0,0 +1,71 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BABall.h"
+
+// Sets default values
+ABABall::ABABall()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	BallComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BallComponent"));
+	RootComponent = BallComponent;
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> SphereAsset(TEXT("/Engine/BasicShapes/Sphere.Sphere"));
+	if (SphereAsset.Succeeded())
+	{
+		BallComponent->SetStaticMesh(SphereAsset.Object);
+	}
+
+	//remove all physics system
+	BallComponent->SetSimulatePhysics(false);
+	BallComponent->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+}
+
+// Called when the game starts or when spawned
+void ABABall::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABABall::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ABABall::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+void ABABall::Jump()
+{
+	if (!CheckValidity()) return;
+
+	BallComponent->AddForce(FVector(0, 0, JumpForce));
+}
+
+void ABABall::Move()
+{
+	if (!CheckValidity()) return;
+
+	BallComponent->AddForce(FVector(0, 0, 0));
+}
+
+bool ABABall::CheckValidity()
+{
+	if (!BallComponent || !BallComponent->IsSimulatingPhysics())
+	{
+		UE_LOG(LogTemp, Error, TEXT("[ABABall::CheckValidity] BallComponent not valid"));
+		return false;
+	}
+
+	return true;
+}
+
---
+++ Source/Roman_BallPlatformer/Public/BABall.h
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "BABall.generated.h"
+
+UCLASS()
+class ROMAN_BALLPLATFORMER_API ABABall : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ABABall();
+
+private:
+	UPROPERTY(VisibleAnywhere, Category = "Move")
+	UStaticMeshComponent* BallComponent;
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Move")
+	float Speed;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Move")
+	float JumpForce;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	void Jump();
+	void Move();
+	bool CheckValidity();
+};
